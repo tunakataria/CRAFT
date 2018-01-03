6349710979237fe5ff21087eea2a4ed4e30d88ba
hitesh <katariah.hitesh@yahoo.in>
2018-01-04 00:49:05 +0530

    Creating Branch

---
+++ testscripts/MobileTestingScenario/GoogleSearch.java
@@ -0,0 +1,51 @@
+package testscripts.MobileTestingScenario;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.cognizant.framework.IterationOptions;
+import com.cognizant.framework.selenium.ExecutionMode;
+import com.cognizant.framework.selenium.MobileExecutionPlatform;
+import com.cognizant.framework.selenium.MobileToolName;
+import com.cognizant.framework.selenium.SeleniumTestParameters;
+
+import supportlibraries.DriverScript;
+import supportlibraries.CRAFTTestCase;
+
+/**
+ * Test for login with invalid user credentials
+ * 
+ * @author Cognizant
+ */
+public class GoogleSearch extends CRAFTTestCase {
+
+	@Test(dataProvider = "TestConfigurations")
+	public void testRunner(String testInstance, ExecutionMode executionMode,
+			MobileToolName mobileToolName,
+			MobileExecutionPlatform executionPlatform, String deviceName) {
+		SeleniumTestParameters testParameters = new SeleniumTestParameters(
+				currentScenario, currentTestcase);
+		testParameters
+				.setCurrentTestDescription("Test for login with invalid user credentials");
+		testParameters.setCurrentTestInstance(testInstance);
+		testParameters.setExecutionMode(executionMode);
+		testParameters.setMobileToolName(mobileToolName);
+		testParameters.setMobileExecutionPlatform(executionPlatform);
+		testParameters.setDeviceName(deviceName);
+		testParameters
+				.setIterationMode(IterationOptions.RUN_ONE_ITERATION_ONLY);
+
+		DriverScript driverScript = new DriverScript(testParameters);
+		driverScript.driveTestExecution();
+
+		tearDownTestRunner(testParameters, driverScript);
+	}
+
+	@DataProvider(name = "TestConfigurations", parallel = true)
+	public Object[][] dataTC2() {
+		return new Object[][] {
+
+		{ "Instance2", ExecutionMode.MOBILE, MobileToolName.APPIUM,
+				MobileExecutionPlatform.WEB_ANDROID, "4d005cb2c4938197" }, };
+	}
+}
